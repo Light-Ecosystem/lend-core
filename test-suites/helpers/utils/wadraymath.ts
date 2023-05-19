b77e1ad7cac21c0584c0b88a5b7e4253889dec62
J-Phx1x <zhiyu.wang@hope.money>
2023-05-19 12:08:08 +0800

    fix raymath utils

--- test-suites/helpers/utils/wadraymath.ts
+++ test-suites/helpers/utils/wadraymath.ts
@@ -32,14 +32,41 @@ declare module '@ethersproject/bignumber' {
 }
 
 BigNumber.prototype.ray = (): BigNumber => BigNumber.from(RAY);
+function ray (): BigNumber {
+  return BigNumber.from(RAY);
+};
+
 BigNumber.prototype.wad = (): BigNumber => BigNumber.from(WAD);
+function wad (): BigNumber {
+  return BigNumber.from(WAD);
+};
+
 BigNumber.prototype.halfRay = (): BigNumber => BigNumber.from(HALF_RAY);
+function halfRay (): BigNumber {
+  return BigNumber.from(HALF_RAY);
+};
+
 BigNumber.prototype.halfWad = (): BigNumber => BigNumber.from(HALF_WAD);
+function halfWad (): BigNumber {
+  return BigNumber.from(HALF_WAD);
+};
+
 BigNumber.prototype.halfPercentage = (): BigNumber => BigNumber.from(HALF_PERCENTAGE);
+function halfPercentage (): BigNumber {
+  return BigNumber.from(HALF_PERCENTAGE);
+};
+
 BigNumber.prototype.percentageFactor = (): BigNumber => BigNumber.from(PERCENTAGE_FACTOR);
+function percentageFactor (): BigNumber {
+  return BigNumber.from(PERCENTAGE_FACTOR);
+};
 
 BigNumber.prototype.wadMul = function (other: BigNumber): BigNumber {
   return this.halfWad().add(this.mul(other)).div(this.wad());
+};
+
+function wadMul (receiver: BigNumber, other: BigNumber): BigNumber {
+  return halfWad().add(receiver.mul(other)).div(wad());
 };
 
 BigNumber.prototype.wadDiv = function (other: BigNumber): BigNumber {
@@ -47,17 +74,35 @@ BigNumber.prototype.wadDiv = function (other: BigNumber): BigNumber {
   return halfOther.add(this.mul(this.wad())).div(other);
 };
 
+function wadDiv (receiver: BigNumber, other: BigNumber): BigNumber {
+  const halfOther = other.div(2);
+  return halfOther.add(receiver.mul(wad())).div(other);
+};
+
 BigNumber.prototype.rayMul = function (other: BigNumber): BigNumber {
   return this.halfRay().add(this.mul(other)).div(this.ray());
 };
+
+function rayMul (receiver: BigNumber, other: BigNumber): BigNumber {
+  return halfRay().add(receiver.mul(other)).div(ray());
+}
 
 BigNumber.prototype.rayDiv = function (other: BigNumber): BigNumber {
   const halfOther = other.div(2);
   return halfOther.add(this.mul(this.ray())).div(other);
 };
 
+function rayDiv (receiver: BigNumber, other: BigNumber): BigNumber {
+  const halfOther = other.div(2);
+  return halfOther.add(receiver.mul(ray())).div(other);
+}
+
 BigNumber.prototype.percentMul = function (bps: BigNumberish): BigNumber {
   return this.halfPercentage().add(this.mul(bps)).div(PERCENTAGE_FACTOR);
+};
+
+function percentMul (receiver: BigNumber, bps: BigNumberish): BigNumber {
+  return halfPercentage().add(receiver.mul(bps)).div(PERCENTAGE_FACTOR);
 };
 
 BigNumber.prototype.percentDiv = function (bps: BigNumberish): BigNumber {
@@ -65,15 +110,51 @@ BigNumber.prototype.percentDiv = function (bps: BigNumberish): BigNumber {
   return halfBps.add(this.mul(PERCENTAGE_FACTOR)).div(bps);
 };
 
+function percentDiv (receiver: BigNumber, bps: BigNumberish): BigNumber {
+  const halfBps = BigNumber.from(bps).div(2);
+  return halfBps.add(receiver.mul(PERCENTAGE_FACTOR)).div(bps);
+};
+
 BigNumber.prototype.rayToWad = function (): BigNumber {
   const halfRatio = BigNumber.from(WAD_RAY_RATIO).div(2);
   return halfRatio.add(this).div(WAD_RAY_RATIO);
+};
+
+function rayToWad (receiver: BigNumber): BigNumber {
+  const halfRatio = BigNumber.from(WAD_RAY_RATIO).div(2);
+  return halfRatio.add(receiver).div(WAD_RAY_RATIO);
 };
 
 BigNumber.prototype.wadToRay = function (): BigNumber {
   return this.mul(WAD_RAY_RATIO);
 };
 
+function wadToRay (receiver: BigNumber): BigNumber {
+  return receiver.mul(WAD_RAY_RATIO);
+}
+
 BigNumber.prototype.negated = function (): BigNumber {
   return this.mul(-1);
+};
+
+function negated (receiver: BigNumber): BigNumber {
+  return receiver.mul(-1);
+};
+
+export {
+  ray,
+  wad,
+  halfRay,
+  halfWad,
+  halfPercentage,
+  percentageFactor,
+  wadMul,
+  wadDiv,
+  rayMul,
+  rayDiv,
+  percentMul,
+  percentDiv,
+  rayToWad,
+  wadToRay,
+  negated,
 };
